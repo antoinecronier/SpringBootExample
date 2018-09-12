4104e313b87d4423632da1a6b21a19b53e80fc6a
Antoine Cronier <antoine.cronier@tactfactory.com>
2018-09-12 11:12:22 +0200

    update build.gradle for bootRun + external doc for war

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/**
+ * For war deploiment see :
+ * 	- https://www.baeldung.com/spring-boot-war-tomcat-deploy
+ * 	- https://tomcat.apache.org/tomcat-8.0-doc/deployer-howto.html
+ * 	- https://askubuntu.com/questions/135824/what-is-the-tomcat-installation-directory
+ * @author tactfactory
+ *
+ */
 public class DemoApplication {
 
 	public static void main(String[] args) {
